b696c6bce0dc920af99acd7420575199aba5c57d
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-01-11 20:28:19 +0700

    install ejs template engine

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send("Welcome to Nodemon"))
+app.set('views', './views') // Specify the views directory
+app.set('view engine', 'ejs') // Register the template engine
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// app.get('/', (req, res) => res.send("Welcome to Nodemon"))
 app.get('/:name', (req, res) => res.send(`Nama saya : ${req.params.name}`) )
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
